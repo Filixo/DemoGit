2778f8e7b34c2468407aff85e9df8aabb1d31300
Filixo <alaeboughaba@gmail.com>
2022-07-22 12:29:08 +0200

    ajout nom ; del test2

--- src/main/java/model/Personne.java
+++ src/main/java/model/Personne.java
@@ -4,9 +4,10 @@ public class Personne {
 
 private String prenom;
 private int age;
+private String nom;
 
 
-public Personne(String prenom, int age) {
+public Personne(String prenom, int age, String nom) {
 	super();
 	this.prenom = prenom;
 	this.age= age;
@@ -33,9 +34,19 @@ public void setAge(int age) {
 }
 
 
+public String getNom() {
+	return nom;
+}
+
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+
 @Override
 public String toString() {
-	return "Personne [prenom=" + prenom + ", age=" + age + "]";
+	return "Personne [prenom=" + prenom + ", age=" + age + ", nom=" + nom + "]";
 }
 
 
--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,7 +5,7 @@ import model.Personne;
 public class Test {
 
 	public static void main(String[] args) {
-		Personne p = new Personne("Alae", 28);
+		Personne p = new Personne("Alae", 28, "Boughaba");
 		System.out.println(p);
 
 	}
