662287da811a62872a539bffbe0ed880b78ff76d
Filixo <alaeboughaba@gmail.com>
2022-07-22 12:13:58 +0200

    Ajout age Personne

--- src/main/java/model/Personne.java
+++ src/main/java/model/Personne.java
@@ -3,25 +3,42 @@ package model;
 public class Personne {
 
 private String prenom;
+private int age;
 
 
-public Personne(String prenom) {
+public Personne(String prenom, int age) {
 	super();
 	this.prenom = prenom;
+	this.age= age;
 }
 
-public String getprenom() {
+
+public String getPrenom() {
 	return prenom;
 }
 
-public void setprenom(String prenom) {
+
+public void setPrenom(String prenom) {
 	this.prenom = prenom;
 }
 
+
+public int getAge() {
+	return age;
+}
+
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+
 @Override
 public String toString() {
-	return "Personne [prenom=" + prenom + "]";
+	return "Personne [prenom=" + prenom + ", age=" + age + "]";
 }
+
+
 	
 	
 }
--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,7 +5,7 @@ import model.Personne;
 public class Test {
 
 	public static void main(String[] args) {
-		Personne p = new Personne("Alae");
+		Personne p = new Personne("Alae", 28);
 		System.out.println(p);
 
 	}
